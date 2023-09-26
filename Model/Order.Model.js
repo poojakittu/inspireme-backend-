892fd45d4ae905f9c7cecb9d7d2684c7a52ad0d8
poojakittu <poojayadav.yadav173@gmail.com>
2023-09-26 22:40:19 +0530

    added

--- Model/Order.Model.js
+++ Model/Order.Model.js
@@ -62,10 +62,7 @@ const orderSchema = new mongoose.Schema(
       ref: "user",
       required: true,
     },
-    cartId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "cart",
-    },
+    
     orderDate: { type: Date, default: Date.now },
   },
   {
